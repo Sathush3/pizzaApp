f66f18cd1bf1d40c2230abec6a7518b78bb4792e
sathush <sathush3@gmail.com>
2022-05-21 19:03:14 +0530

    Initial Commit

---
+++ pizzaApp/pizzaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  pizzaAppApp.swift
+//  pizzaApp
+//
+//  Created by Sathurshan Ramachandran on 2022-05-21.
+//
+
+import SwiftUI
+
+@main
+struct pizzaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
